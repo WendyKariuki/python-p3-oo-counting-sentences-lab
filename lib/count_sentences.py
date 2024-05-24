73e96d64dc78b4f048a99e64e37905e1fc206b92
WendyKariuki <phaminuswendy@gmail.com>
2024-05-24 16:56:32 +0300

    OO counting sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
-#!/usr/bin/env python3
-
+# # # #!/usr/bin/env python3
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self.__value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+      if not self.value:
+          return 0 
+      sentence_ends = [".", "!", "?"]
+      count = 0
+      prev_char = ''
+      for char in self.value:
+          if char in sentence_ends and prev_char not in sentence_ends:
+              count += 1
+          prev_char = char
+      print("Intermediate count:", count)
+      return count
